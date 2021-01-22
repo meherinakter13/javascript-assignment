1e9216a85f1a9a4d7a9a2e9436181f448fc01ce0
Meherin Akter <meherinakter13@gmail.com>
2021-01-22 15:21:26 +0600

    syntax alignment

--- assignment.js
+++ assignment.js
@@ -1,12 +1,16 @@
+//  https://github.com/meherinakter13/javascript-assignment
 
 // Convert kilometer to meter
-function kilometerToMeter(kilometer){
-    if(kilometer < 0){
+function kilometerToMeter(kilometer)
+{
+    if(kilometer < 0)
+    {
         console.log('You have given an invalid input');
     }
-    else{
-    var meter = kilometer * 1000;
-    return meter;
+    else
+    {
+        var meter = kilometer * 1000;
+        return meter;
     }
 }
 
@@ -16,16 +20,19 @@ console.log(result);*/
 
 
 // Budget calculation
-function budgetCalculator(watch,phone,laptop){
-    if(watch < 0 || phone < 0 || laptop <0){
+function budgetCalculator(watch,phone,laptop)
+{
+    if(watch < 0 || phone < 0 || laptop <0)
+    {
         console.log('You have given an invalid input');
     }
-    else{
-    costOfWatch = watch * 50;
-    costOfphone = phone * 100;
-    costOfLaptop = laptop * 500;
-    totalCost = costOfWatch + costOfphone + costOfLaptop;
-    return totalCost;
+    else
+    {
+        costOfWatch = watch * 50;
+        costOfphone = phone * 100;
+        costOfLaptop = laptop * 500;
+        totalCost = costOfWatch + costOfphone + costOfLaptop;
+        return totalCost;
     }
 }
 /*var result = budgetCalculator(2,5,2);
@@ -34,48 +41,57 @@ console.log(result);*/
 
 
 // Finding the Hotelcost
-function hotelCost(day){
+function hotelCost(day)
+{
     var cost = 0;
-    if(day < 0){
+    if(day < 0)
+    {
         console.log('You have given an invalid input');
     }
-    else if(day <= 10){
+    else if(day <= 10)
+    {
         cost = day * 100;
         return cost;
     }
-    else if(day <= 20){
+    else if(day <= 20)
+    {
         var firstpart = 10 * 100;
         var remainingDay = day - 10;
         var secondpart = remainingDay * 80;
         cost = firstpart + secondpart;
         return cost;
     }
-    else{
+    else
+    {
         var firstpart = 10 * 100;
         var secondpart = 10 * 80;
         var remainingDay = day - 20;
         var thirdpart = remainingDay * 50;
         cost = firstpart + secondpart + thirdpart;
         return cost;
-    }
-    
+    }   
 }
 /*var result = hotelCost(11);
 console.log(result);*/
 
 
 
-//finding the largest name
+//finding the largest name from an array
 
-function megaFriend(friendsName){
-    if(friendsName <= 0){
+function megaFriend(friendsName)
+{
+    if(friendsName <= 0)
+    {
         console.log('You have given an invalid input');
     }
-    else{
+    else
+    {
         var largeName = friendsName[0];
-        for(var i = 0; i < friendsName.length; i++){
+        for(var i = 0; i < friendsName.length; i++)
+        {
             var element = friendsName[i];
-            if(element.length > largeName.length){
+            if(element.length > largeName.length)
+            {
                largeName = element;
             }
         }
